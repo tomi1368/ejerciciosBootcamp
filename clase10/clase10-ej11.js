5d8772e8d85c8025ab2671912bb089c42aad5ea2
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2021-11-05 12:32:17 -0300

    contemplo casos negativos clase10 ej11

--- clase10/clase10-ej11.js
+++ clase10/clase10-ej11.js
@@ -1,4 +1,11 @@
-const lessDist = (num1,num2)=> ((100 - num1) > (100-num2)) ? num2 : num1
+const lessDistPos = (num1,num2)=> ((100 - num1) > (100-num2)) ? num2 : num1
 
+const lessDistNeg = (num1,num2) => ((100 - Math.abs(num1)) > (100- Math.abs(num2))) ? num1 : num2
 
-
+const m = (num1,num2)=>{
+    if (num1 === num2) return "son el mismo num"
+    else if (Math.sign(num1) === -1 && Math.sign(num2) === -1) return lessDistNeg(num1,num2)
+    else if(Math.sign(num1) === -1) return num2
+    else if(Math.sign(num2) === -1) return num1
+    else return lessDistPos(num1,num2)
+}
