62dee40e3554a2cf7db2049e317ad41edd908e02
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2021-11-08 13:11:36 -0300

    Terminado Clase11-ej1

--- clase10/clase10-ej5.js
+++ clase10/clase10-ej5.js
@@ -1,5 +1,17 @@
-const invert = arr =>{
+/* const invert = arr =>{
     let array=[...arr]
     return array.reverse()
+} */
+/* let arr = ["2","3"]
+function concaternarArray1(array1,array2){
+
+    for(let i=0;i<array2.length;i++){
+        array1.push(array2[i]);
+    }
+   
+    return console.log(array1)
 }
 
+concaternarArray1(arr,["4","5"])
+
+console.log(arr) me lo modifica */
---
+++ clase10/teoria.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3]
+
+
+
+
+
+/* for(let i = 0, len = arr.length; i<len;i++){
+    console.log(arr[i])
+} */
+
+
