b8d0f93fb4df4f6fb3677cd7db5f15bb9dd1db35
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2021-11-09 13:17:00 -0300

    "clase12-ej3" el pongo +1

--- clase12/clase12-parte1/clase12-ej3.js
+++ clase12/clase12-parte1/clase12-ej3.js
@@ -1,6 +1,6 @@
 const random = ()=>{
     let num = prompt("Adivina del 1 al 10"),
-    numRandom = Math.floor(Math.random()*10)
+    numRandom = Math.floor(Math.random()*10 + 1)
     if (num === numRandom ) return "acertaste"
     else random()
 }
